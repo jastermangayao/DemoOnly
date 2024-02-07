e0015b01c4f74af13897cb44172a6080692da77c
User <User@192.168.0.126>
2024-02-07 14:13:30 +0800

    version2, naaay scanner

---
+++ src/pkgpublic/pkgclass/PublicClass.java
@@ -0,0 +1,23 @@
+
+package pkgpublic.pkgclass;
+
+
+public class PublicClass {
+
+   
+    
+        
+        public static int addNumbers(int a, int b){
+            return a + b;
+           
+        } 
+        
+        public static void main(String[] args) {
+            //Calling the method with return type int
+            int result = addNumbers(5, 3);
+            System.out.println("Resul: " + result);
+        }
+    }
+
+    
+
